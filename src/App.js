9a45a96fcf04a46b7c5529dd30292ece7501fc9d
CodeModeOn <info.zaidarif30@gmail.com>
2025-06-01 10:46:57 +0500

    dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title="welcome to new blog"
+
   return (
     <div className="App">
       <div className="container">
-        <h1>App Container</h1>
+        <h1> {title} </h1>
+        <p>10</p>
+        <p>"hello ninjas</p>
+        <p>Math.random() * 10
+          
+        </p>
+        
       </div>
     </div>
   );
