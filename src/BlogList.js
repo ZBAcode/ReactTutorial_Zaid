a1844ed6841a1e046c10b7a42eb674d7f46dd658
CodeModeOn <info.zaidarif30@gmail.com>
2025-06-16 23:56:00 +0500

    function as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,5 @@
-const BlogList = (props) => {
-    const blogs=props.blogs;
-    const title=props.title;
+const BlogList = ({blogs,title,handleDelete}) => {
+    
 
 
     return (
@@ -10,6 +9,7 @@ const BlogList = (props) => {
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
+          <button onClick={()=>handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}       
         </div>
